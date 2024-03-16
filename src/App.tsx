fdcf4f59b5f03c7a4485501e89a31f974e3b2550
ipi19 <ipyanamwakikunga@gmail.com>
2024-03-16 09:47:42 +0200

    buildTheColorModeSwitch

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
   base : `"nav" " main"`,
   lg: `"nav nav"  "aside main"`
  }}>
- <GridItem area='nav'><NavBar/></GridItem>
+ <GridItem area='nav' ><NavBar/></GridItem>
 
- <Show above='lg'><GridItem area='aside' bg='gold'>Aside</GridItem></Show>
+ <Show above='lg'><GridItem area='aside' >Aside</GridItem></Show>
 
  
  
- <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+ <GridItem area='main'>Main</GridItem>
  </Grid>
 
  );
--- src/componets/NavBar.tsx
+++ src/componets/NavBar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, } from "@chakra-ui/react";
 import logo from       "../assets/logo.webp";
+import COlorModeSwitch from "./ColorModeSwitch";
 
 
 
 const NavBar = () => {
     return (
-            <HStack>
+            <HStack justifyContent='space-between' padding='10px'>
               <Image src= {logo} boxSize='60px'/>
-              <Text>NavBAr</Text>
+          <COlorModeSwitch/>          
             </HStack>
         )
 }
