8ec03549809eb91c5361d43aa6a753b76923c771
ipi19 <ipyanamwakikunga@gmail.com>
2024-03-18 20:53:52 +0200

    optimized images 4

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,7 +4,7 @@ const target = 'media/';
 
   const index = url.indexOf(target)+target.length;
 
-  return url.slice(0, index) + 'crop/600/400' + url.slice(index)
+  return (url.slice(0, index) + 'crop/600/400' + url.slice(index))
 
 }
 
